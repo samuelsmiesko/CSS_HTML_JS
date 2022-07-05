b82ebbfd42795aeaaca916b42c4d69881f98a933
samuelsmiesko <samuel1820@gmail.com>
2022-07-05 18:40:22 +0200

    Add files via upload
    This is script for Page.html

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Coffe has 0,5 kcal";
+}
+
+function my2ndFunction() {
+  document.getElementById("demo").innerHTML = "Coffe has 2 kcal";
+}
