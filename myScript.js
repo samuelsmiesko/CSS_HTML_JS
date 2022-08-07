8ca8fc5ed56b19d1de5a206e85d1d6fc3670764b
samuelsmiesko <samuel1820@gmail.com>
2022-08-07 20:46:11 +0200

    Delete myScript.js

--- myScript.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Coffe has 0,5 kcal";
-}
-
-function my2ndFunction() {
-  document.getElementById("demo").innerHTML = "Coffe has 2 kcal";
-}
